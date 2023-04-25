47b70a5cc7e980fa650a84111f5dd58ff15574bc
eefesafak <79419141+eefesafak@users.noreply.github.com>
2023-04-26 02:59:37 +0300

    Update Lesson 1.cpp

--- 1/Lesson 1/Lesson 1/Lesson 1.cpp
+++ 1/Lesson 1/Lesson 1/Lesson 1.cpp
@@ -3,30 +3,30 @@ using namespace std;
 
 int main()
 {
-	// differences about const in c and c++
-	// const int x; no error in C
-	// const int x; syntax error in c++
+	 differences about const in c and c++
+	 const int x; no error in C
+	 const int x; syntax error in c++
 
 //---------------------------
 	
-	//int x = 10;
-	//int y = 20;
-	//int* const p = &x; // const pointer- top level const
-	//*p = 45; // true
-	//p = &y; // false
+	int x = 10;
+	int y = 20;
+	int* const p = &x; // const pointer- top level const
+	*p = 45; // true
+	p = &y; // false
 
 //---------------------------
 
-	// int x = 10;
-	// int y = 20;
-	// int* p = &x; // low level const (pointer to const int)
-	// *p = 56; //false
-	// p = &y; // no error
+	 int x = 10;
+	 int y = 20;
+	 int* p = &x; // low level const (pointer to const int)
+	 *p = 56; //false
+	 p = &y; // no error
 
 //---------------------------
 	
-	//int* const p; error
-	//const int* p; no error
+	int* const p; error
+	const int* p; no error
 
 //---------------------------
 
